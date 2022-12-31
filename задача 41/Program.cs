69bdda2fd57fe37db268d0c6ad032906ae16d19f
semen <vizitagent@mail.ru>
2022-12-31 13:05:28 +0900

    first commit

---
+++ задача 41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
